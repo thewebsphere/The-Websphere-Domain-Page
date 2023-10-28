8e4a008f07af8aa1d9052f67ab19c36055b11138
Muthung <muthunguclintn@gmail.com>
2023-10-28 08:26:50 +0300

    updated on 27/10/23

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const header = document.querySelector('.homeName');
+const home = document.querySelector('.homeText');
+const homeContainer = document.querySelector('#Home');
+let prevScrollY = window.scrollY;
 
 // Update font size based on scroll position
 function updateScale() {
@@ -7,4 +10,27 @@ function updateScale() {
     header.style.transform = `scale(${newScale})`;
 }
 
-window.addEventListener('scroll', updateScale);
+//  Move the homeText down
+function scrollDown() {
+    const scrollY = window.scrollY;
+    const deltaY = scrollY - prevScrollY;
+    const transformValue = `translateY(${deltaY * 2}px)`;
+    home.style.transform = transformValue;
+    prevScrollY = scrollY;
+}
+
+// Reduce the opacity on scrolling up
+function opacityReduction() {
+    const scrollY = window.scrollY;
+    if (scrollY < prevScrollY) {
+        const newOpacity = 1 - scrollY * 0.5;
+        homeContainer.style.opacity = newOpacity;
+    }
+    prevScrollY = scrollY;
+}
+
+window.addEventListener('scroll', () => {
+    updateScale();
+    scrollDown();
+    opacityReduction();
+});
