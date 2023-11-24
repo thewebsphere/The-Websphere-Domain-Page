905f5a62c7b67c534e851766067fbb5ac70968aa
Muthung <muthunguclintn@gmail.com>
2023-11-24 14:45:00 +0300

    updated on 24/11/23

---
+++ Projects/projects.js
@@ -0,0 +1,6 @@
+var ProjectImg = document.getElementById('projectsImage');
+var ProjectBrief = document.getElementById('projectsBrief')
+
+setTimeout(function()  {
+    ProjectImg.style.opacity = 0;
+}, 7000);
