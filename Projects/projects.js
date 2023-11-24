c7b51ea360d046ee1c7b56cbd5c156dfe80a96b9
Muthung <muthunguclintn@gmail.com>
2023-11-24 14:54:28 +0300

    updated on 24/11/23

--- Projects/projects.js
+++ Projects/projects.js
@@ -1,6 +1,18 @@
 var ProjectImg = document.getElementById('projectsImage');
 var ProjectBrief = document.getElementById('projectsBrief')
 
+var MBrief = document.getElementById('MuthunguBrief');
+var MImg = document.getElementById('MuthunguImage');
+
+var CBrief = document.getElementById('companyBrief');
+var CImg = document.getElementById('companyImage');
+
+var SBrief = document.getElementById('storeBrief');
+var SImg = document.getElementById('storeImage');
+
 setTimeout(function()  {
-    ProjectImg.style.opacity = 0;
-}, 7000);
+    ProjectImg.style.display = 'none';
+    ProjectBrief.style.display = 'none';
+    MImg.style.display = 'block';
+    MBrief.style.display = 'block';
+}, 4200);
