a33a7d3b4b2161a0c731bf8e37422f2866d23b01
Muthung <muthunguclintn@gmail.com>
2023-11-28 10:49:55 +0300

    updated on 28/11/23

--- Projects/projects.js
+++ Projects/projects.js
@@ -1,18 +1,38 @@
-var projectsImages = ['projectsImage', 'MuthunguImage', 'companyImage', 'storeImage'];
-var projectsBriefs =['projectsBrief', 'MuthunguBrief', 'companyBrief', 'storeBrief'];
 
-function nextProject(index) {
-    document.getElementById(projectsImages[index]).style.display = 'none';
-    document.getElementById(projectsBriefs[index]).style.display = 'none';
+var ProjectImg = document.getElementById('projectsImage');
+var ProjectBrief = document.getElementById('projectsBrief');
 
-    var nextIndex = (index + 1) % projectsImages.length;
+var MBrief = document.getElementById('MuthunguBrief');
+var MImg = document.getElementById('MuthunguImage');
 
-    document.getElementById(projectsImages[nextIndex]).style.display = 'block';
-    document.getElementById(projectsBriefs(nextIndex)).style.display = 'block';
+var CBrief = document.getElementById('companyBrief');
+var CImg = document.getElementById('companyImage');
+
+var SBrief = document.getElementById('storeBrief');
+var SImg = document.getElementById('storeImage');
+
+function transitionProjects(imgShown, briefShown, imgHiden, briefHidden) {
+    imgHiden.style.display = 'none';
+    briefHidden.style.display = 'none';
+    imgShown.style.display = 'block';
+    briefShown.style.display = 'block';
 }
 
-nextProject(0);
-
-var timerId = setInterval(function() {
-    nextProject(projectsImages.indexOf('projectsImage'));
+setTimeout(function () {
+    transitionProjects(MImg, MBrief, ProjectImg, ProjectBrief);
 }, 4200);
+
+setTimeout(function () {
+    transitionProjects(CImg, CBrief, MImg, MBrief);
+}, 8400);
+
+setTimeout(function () {
+    transitionProjects(SImg, SBrief, CImg, CBrief);
+}, 12600);
+
+setTimeout(function () {
+    SImg.style.display = 'none';
+    SBrief.style.display = 'none'
+    ProjectImg.style.display = 'block';
+    ProjectBrief.style.display = 'block';
+}, 16800);
