fce32cca0b8829b7959b3ee57db832a599759260
Muthung <muthunguclintn@gmail.com>
2024-11-17 14:13:30 +0300

    Update: smooth scrolling effect.

--- main.js
+++ main.js
@@ -17,3 +17,21 @@ window.addEventListener('hashchange', highLight);
 
 // Initial call to highlight the current page onload
 highLight();
+
+// Smooth Scroll on Click
+function smoothScroll(event) {
+    event.preventDefault();
+    const targetId = event.currenTarget.getAttribute("href").substring(1);
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: "smooth"
+        });
+    }
+}
+
+// Event Listener for smooth scroll
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', smoothScroll);
+});
